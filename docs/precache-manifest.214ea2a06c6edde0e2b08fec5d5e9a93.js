3d3479c911085c55705babfc327baa3d95bf1194
Akshita <akshitaverma102001@gmail.com>
2020-11-16 23:11:58 +0530

    commit

---
+++ docs/precache-manifest.214ea2a06c6edde0e2b08fec5d5e9a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2054cc53ef27ee75f772",
+    "url": "/portfolio/static/css/main.1c0c8dd1.chunk.css"
+  },
+  {
+    "revision": "2054cc53ef27ee75f772",
+    "url": "/portfolio/static/js/main.2054cc53.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "316e3574a151ebec916e6c5c60645aa2",
+    "url": "/portfolio/index.html"
+  }
+];
